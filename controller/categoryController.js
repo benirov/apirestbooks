20a03550e18c0ef924268758dc79a4b4ebd9b409
Develov <beniro_vielma@hotmail.com>
2019-06-23 21:41:06 -0500

    cambios en archivos

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,22 +1,26 @@
 'use strict';
-const Category = require('../model/category');
+// const Category = require('../model/category');
+
+const mongoose = require('mongoose');
 
 
 function getCategory(req, res)
 {	
 
-	console.log(Category);
-	Category.find({}, (error, category) =>
-	{
-		if(error)
+	// console.log(Category);
+	mongoose.connection.db.collection("category", function (err, collection) {
+		collection.find({}).toArray(function(error, category)
 		{
-			return res.status(500).send({message: `error al realizar la peticiòn: ${error}`});
-		}
-		else
-		{
-			return res.status(200).send(category)
-		}
+			if(error)
+			{
+				return res.status(500).send({message: `error al realizar la peticiòn: ${error}`});
+			}
+			else
+			{
+				return res.status(200).send(category)
+			}
 		
+		});
 	});
 }
 
