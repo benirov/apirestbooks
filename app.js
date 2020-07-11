373f1e4c0352a20e3e81a9d24ea43f7d5ae79766
beniro <beniro.vielma@zoomcem.com>
2020-07-11 13:00:40 -0500

    make it better

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ const api = require('./route');
 
 app.use(bodyParser.urlencoded({extended : false}));  
 app.use(bodyParser.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use('/api', api);
 
