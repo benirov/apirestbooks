e687fbcb341d31ffa8938aea1f098a5acd2d8555
beniro <beniro.vielma@zoomcem.com>
2020-07-11 13:09:47 -0500

    make it better

--- controller/bookController.js
+++ controller/bookController.js
@@ -291,7 +291,7 @@ function updateBook(req, res)
 	
 	let id = req.params.id;
   	let update = req.body;
-  	let data = { lccn: req.body.lccn };
+  	let data = { image: req.body.image };
   	console.log('id', id);
 	console.log('data', data);
 	var ObjectID = require('mongodb').ObjectID;
