93531e547c20d5a66b7e4bd38792610256ab5566
beniro <beniro.vielma@zoomcem.com>
2020-08-29 14:02:08 -0500

    arreglo de url parser

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 module.exports =
 {
   port: process.env.PORT || 31387,
-  db: process.env.MONGODB_URI || 'mongodb://benirovielma:be.37/22@ds231387.mlab.com/apibooks',
+  db: process.env.MONGODB_URI || 'mongodb://default:default123@ds231387.mlab.com/apibooks',
   SECRET_TOKEN : 'mykeytoken'
 }
